2bd0ad4d38ae888ebc5dda9edaf41d44fc6f26c5
Shubham Gupta <shubhcs01@gmail.com>
2024-07-18 23:57:04 +0530

    feat: added mobile header & media queries

--- uolo-gurukul-frontend/src/components/Header/Header.js
+++ uolo-gurukul-frontend/src/components/Header/Header.js
@@ -1,16 +1,20 @@
 import "./Header.css";
 import Logo from "../../assets/uoloLogo.png";
 import AvatarSection from "../Avatar/AvatarSection";
-import {useNavigate} from 'react-router-dom';
+import { useNavigate } from "react-router-dom";
 
 const Header = () => {
-
-  const navigate = useNavigate()
+  const navigate = useNavigate();
 
   return (
     <div className="header sticky">
-      <img className="uolo-logo" src={Logo} alt="uoloLogo" onClick={()=>navigate('/')} />
-      <AvatarSection/>
+      <img
+        className="uolo-logo"
+        src={Logo}
+        alt="uoloLogo"
+        onClick={() => navigate("/")}
+      />
+      <AvatarSection />
     </div>
   );
 };
--- uolo-gurukul-frontend/src/components/Header/MobileHeader.js
+++ uolo-gurukul-frontend/src/components/Header/MobileHeader.js
@@ -2,11 +2,12 @@ import "./Header.css";
 import Logo from "../../assets/uoloLogo.png";
 import {useNavigate} from 'react-router-dom';
 import { IoMenu } from "react-icons/io5";
-import { CgProfile } from "react-icons/cg";
+import { useAuth } from "../Auth/authContext";
 
 const MobileHeader = ({showHamburger, setShowHamburger}) => {
 
   const navigate = useNavigate()
+  const { currentUser } = useAuth();
 
   const Hamburger = <IoMenu className="HamburgerMenu"
   size="30px" color="black" onClick={() => setShowHamburger(!showHamburger)}/>
@@ -15,7 +16,7 @@ const MobileHeader = ({showHamburger, setShowHamburger}) => {
     <div className={showHamburger? "hide-header":"mobile-header sticky"}>
         {Hamburger}
       <img className="uolo-logo" src={Logo} alt="uoloLogo" onClick={()=>navigate('/')} />
-      <CgProfile className="mobile-avatar" size="30px"/>
+      <img className="avatar" style={{marginRight:"10px"}} src={currentUser.imageUrl} alt="userAvatar" />
     </div>
   );
 };
